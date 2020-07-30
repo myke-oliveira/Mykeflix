f11755027a86cbd4cd395eb28abba7f1553562b2
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2020-07-30 00:34:31 -0300

    cadastro categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,24 +1,79 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import PageDefault from '../../../components/PageDefault';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import PageDefault from "../../../components/PageDefault";
+// import FormField from "../../../components/FormFields";
 
-function CadastroCadastro() {
+function CadastroCategoria() {
+  const valoresIniciais = {
+    nome: "",
+    descricao: "",
+    cor: "#000",
+  };
+
+  const [values, setValues] = useState(valoresIniciais);
+  const [lista, setLista] = useState([]);
+
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor,
+    });
+  }
+
+  function handleChange(e) {
+    setValue(
+      e.target.getAttribute('name'),
+      e.target.value
+    );
+  }
+
   return (
     <PageDefault>
-      <h1>Cadastro de Cadastro</h1>
-
-      <form>
-        Nome da Categoria:
-        <input type="text"/>
-        <button>
-          Cadastrar
-        </button>
+      <h1>Cadastro de Categoria: {values.nome}</h1>
+      <form
+        onSubmit={(infosDoEvento) => {
+          infosDoEvento.preventDefault();
+          setLista([...lista, values]);
+          setValues(valoresIniciais);
+        }}
+      >
+        <div>
+          <label>Nome da Categoria:</label>
+          <input
+            type="text"
+            name="nome"
+            value={values.nome}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Descrição:</label>
+          <textarea
+            type="text"
+            name="descricao"
+            value={values.descricao}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Cor:</label>
+          <input
+            type="color"
+            name="cor"
+            value={values.cor}
+            onChange={handleChange}
+          />
+        </div>
+        <button>Cadastrar</button>
       </form>
-      <Link to="/">
-        Ir para home
-      </Link>
+      <ul>
+        {lista.map((value, indice) => (
+          <li key={`${value.nome}${indice}`}>{value.nome}</li>
+        ))}
+      </ul>
+      <Link to="/">Ir para home</Link>
     </PageDefault>
   );
 }
 
-export default CadastroCadastro;
+export default CadastroCategoria;
