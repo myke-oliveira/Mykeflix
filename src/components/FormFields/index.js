307439346f309d2c9dc7fdcbd188d61e85d69b22
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2020-08-02 16:32:22 -0300

    eslint e prototypes

--- src/components/FormFields/index.js
+++ src/components/FormFields/index.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function FormField({ label, type, name, value, onChange }) {
+function FormField({
+  label, type, name, value, onChange,
+}) {
+  const fieldId = `id_${name}`;
+
   return (
     <div>
-      <label>{label}</label>
+      <label htmlFor={fieldId}>{label}</label>
       <input
+        id={fieldId}
         type={type}
         name={name}
         value={value}
@@ -13,5 +19,19 @@ function FormField({ label, type, name, value, onChange }) {
     </div>
   );
 }
+
+FormField.defaultProps = {
+  type: 'text',
+  value: '',
+  onChange: () => {},
+};
+
+FormField.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
 
 export default FormField;
