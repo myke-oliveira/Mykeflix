7282d67e8e74328cb8450ac429c8408c8b2337e1
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2020-08-02 18:03:09 -0300

    animação formulário

--- src/components/FormFields/index.js
+++ src/components/FormFields/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
 
-const FormFiledWrapper = styled.div`
+const FormFieldWrapper = styled.div`
   position: relative;
+
   textarea {
     min-height: 150px;
   }
+
   input[type="color"] {
     padding-left: 56px;
   }
@@ -14,7 +16,23 @@ const FormFiledWrapper = styled.div`
 
 const Label = styled.label``;
 
-Label.Text = styled.span``;
+Label.Text = styled.span`
+  color: #E5E5E5;
+  height: 57px;
+  position: absolute; 
+  top: 0;
+  left: 16px;
+  
+  display: flex;
+  align-items: center;
+  
+  transform-origin: 0% 0%;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 300;
+  
+  transition: .1s ease-in-out;
+`;
 
 const Input = styled.input`
   background: #53585D;
@@ -39,58 +57,58 @@ const Input = styled.input`
   &:focus {
     border-bottom-color: var(--primary);
   }
+
   &:focus:not([type='color']) + ${Label.Text} {
     transform: scale(.6) translateY(-10px);
   }
+
   ${({ value }) => {
     const hasValue = value.length > 0;
     return hasValue && css`
-      &::not()([type='color'] + ${Label.Text} {
-        transform: scale(.6) translateY(-10px);
-      })
-    `;
-  }}
+        &:not([type='color']) + ${Label.Text} {
+          transform: scale(.6) translateY(-10px);
+        }
+      `;
+  }
 }
 `;
 
 function FormField({
   label, type, name, value, onChange,
 }) {
-  const fieldId = `id_${name}`;
-  const isTextArea = type === 'textarea';
-  const tag = isTextArea ? 'textarea' : 'input';
+  const isTypeTextArea = type === 'textarea';
+  const tag = isTypeTextArea ? 'textarea' : 'input';
 
   return (
-    <FormFiledWrapper>
-      <Label htmlFor={fieldId}>
+    <FormFieldWrapper>
+      <Label>
+        <Input
+          as={tag}
+          type={type}
+          value={value}
+          name={name}
+          onChange={onChange}
+        />
         <Label.Text>
           {label}
+          :
         </Label.Text>
       </Label>
-      <Input
-        as={tag}
-        id={fieldId}
-        type={type}
-        name={name}
-        value={value}
-        onChange={onChange}
-      />
-    </FormFiledWrapper>
+    </FormFieldWrapper>
   );
 }
 
 FormField.defaultProps = {
-  type: 'text',
-  value: '',
-  onChange: () => {},
+  type: 'text',
+  value: '',
 };
 
 FormField.propTypes = {
   label: PropTypes.string.isRequired,
-  type: PropTypes.string,
   name: PropTypes.string.isRequired,
+  type: PropTypes.string,
   value: PropTypes.string,
-  onChange: PropTypes.func,
+  onChange: PropTypes.func.isRequired,
 };
 
 export default FormField;
